93bdb75d46260697d23c7977f3856ba0a0d847ab
Dsys1129 <dsys1129@gmail.com>
2023-11-04 15:33:40 +0900

    feat: book Controller 생성

--- src/main/java/com/example/seo_dot/book/controller/BookController.java
+++ src/main/java/com/example/seo_dot/book/controller/BookController.java
@@ -1,4 +1,34 @@
 package com.example.seo_dot.book.controller;
 
+import com.example.seo_dot.book.dto.request.PageParam;
+import com.example.seo_dot.book.dto.response.BookDetailResponseDTO;
+import com.example.seo_dot.book.dto.response.BookListResponseDTO;
+import com.example.seo_dot.book.dto.response.PageDto;
+import com.example.seo_dot.book.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/api/books/{bookId}")
+    public ResponseEntity<BookDetailResponseDTO> getBookDetail(@PathVariable Long bookId) {
+        BookDetailResponseDTO response = bookService.getBookDetail(bookId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/api/category")
+    public ResponseEntity<PageDto<List<BookListResponseDTO>>> getCategoryList(@ModelAttribute PageParam pageParam) {
+        Slice<BookListResponseDTO> result = bookService.getBookList(pageParam);
+        PageDto<List<BookListResponseDTO>> response = new PageDto<>(result.getContent(), result.hasNext());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
