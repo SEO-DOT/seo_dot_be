d4e445c1e0ee2bf402dd2604b4920062586a447b
Dsys1129 <dsys1129@gmail.com>
2023-11-05 19:40:40 +0900

    feat: Review Like 뼈대 구현

---
+++ src/main/java/com/example/seo_dot/review/repository/ReviewLikeRepository.java
@@ -0,0 +1,14 @@
+package com.example.seo_dot.review.repository;
+
+import com.example.seo_dot.review.domain.ReviewLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ReviewLikeRepository  extends JpaRepository<ReviewLike, Long> {
+    @Query("SELECT rl.review.id FROM ReviewLike rl WHERE rl.user.id = :userId And rl.review.bookId = :bookId")
+    List<Long> findReviewIdsByUserIdANDBookId(Long userId, Long bookId);
+
+    ReviewLike findReviewLikeByReviewIdAndUserId(Long id, Long userId);
+}
---
+++ src/main/java/com/example/seo_dot/review/service/ReviewLikeService.java
@@ -0,0 +1,39 @@
+package com.example.seo_dot.review.service;
+
+import com.example.seo_dot.book.repository.BookRepository;
+import com.example.seo_dot.global.dto.MessageResponseDTO;
+import com.example.seo_dot.review.domain.Review;
+import com.example.seo_dot.review.domain.ReviewLike;
+import com.example.seo_dot.review.repository.ReviewLikeRepository;
+import com.example.seo_dot.review.repository.ReviewRepository;
+import com.example.seo_dot.user.domain.User;
+import com.example.seo_dot.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ReviewLikeService {
+
+    private final ReviewLikeRepository reviewLikeRepository;
+    private final BookRepository bookRepository;
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+
+    public MessageResponseDTO reviewLike(Long bookId, Long reviewId, Long userId) {
+        bookRepository.findById(bookId).orElseThrow(() -> new IllegalArgumentException());
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException());
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException());
+        ReviewLike like = reviewLikeRepository.findReviewLikeByReviewIdAndUserId(review.getId(), userId);
+
+        if (like != null) {
+            review.decreaseLikeCount();
+            reviewLikeRepository.delete(like);
+
+        } else {
+            review.increaseLikeCount();
+            reviewLikeRepository.save(new ReviewLike(user, review));
+        }
+        return MessageResponseDTO.createSuccessMessage200();
+    }
+}
