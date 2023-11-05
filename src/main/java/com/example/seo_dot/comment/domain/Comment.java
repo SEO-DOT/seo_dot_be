249de71653e7661b3fce3a31980ab29817f46968
Dsys1129 <dsys1129@gmail.com>
2023-11-05 19:32:38 +0900

    feat: Entity 생성 Review, Comment, ReviewLike

--- src/main/java/com/example/seo_dot/comment/domain/Comment.java
+++ src/main/java/com/example/seo_dot/comment/domain/Comment.java
@@ -1,11 +1,48 @@
 package com.example.seo_dot.comment.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.example.seo_dot.comment.dto.request.CommentCreateRequestDTO;
+import com.example.seo_dot.global.entity.BaseEntity;
+import com.example.seo_dot.review.domain.Review;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
 @Entity
-public class Comment {
+public class Comment extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    private Long userId;
+
+    private String contents;
+
+    private boolean deleted;
+
+    private Comment(Review review, Long userId, String contents) {
+        this.review = review;
+        this.userId = userId;
+        this.contents = contents;
+    }
+
+    public static Comment createComment(Review review, Long userId, CommentCreateRequestDTO requestDTO) {
+        return new Comment(review, userId, requestDTO.getContents());
+    }
+
+    public void updateComment(CommentModifyRequestDTO requestDTO) {
+        this.contents = requestDTO.getContents();
+    }
+
+    public void deleteComment() {
+        this.deleted = true;
+    }
 }
