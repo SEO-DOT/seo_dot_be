26c152453017ff845d132b3c5e4514eb1e47ad48
Dsys1129 <dsys1129@gmail.com>
2023-11-05 19:37:50 +0900

    feat: Review RequestDTO 생성

---
+++ src/main/java/com/example/seo_dot/review/dto/request/ReviewCreateRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.seo_dot.review.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class ReviewCreateRequestDTO {
+
+    private String contents;
+
+    private Integer score;
+}
---
+++ src/main/java/com/example/seo_dot/review/dto/request/ReviewPageParam.java
@@ -0,0 +1,23 @@
+package com.example.seo_dot.review.dto.request;
+
+public class ReviewPageParam {
+
+    private Integer page = 1;
+    private Integer per = 20;
+    private String sort = "new";
+
+    public Integer getPage() {
+        if (page == 0) {
+            return page;
+        }
+        return this.page - 1;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public Integer getPer() {
+        return per;
+    }
+}
