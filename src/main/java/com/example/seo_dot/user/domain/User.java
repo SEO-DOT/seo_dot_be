c1da4875fe965c21575e57cd19958accb418f437
kki4504 <rhkddlf9876@gmail.com>
2023-11-07 06:15:54 +0900

    feat: user entity

--- src/main/java/com/example/seo_dot/user/domain/User.java
+++ src/main/java/com/example/seo_dot/user/domain/User.java
@@ -1,13 +1,39 @@
 package com.example.seo_dot.user.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.example.seo_dot.user.domain.enums.Role;
+import com.example.seo_dot.user.domain.enums.UserType;
+import com.nimbusds.openid.connect.sdk.claims.Gender;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
+@Getter
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private String address;
+    private Date birth;
+    private Gender gender;
+    private int age;
+    private boolean activated;
+    private Role role;
+    private LocalDateTime deleted;
+    private UserType userType;
+
 }
