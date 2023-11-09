7af79b2b68a1cedb6a6727e325968fdf4594a089
Dsys1129 <dsys1129@gmail.com>
2023-11-10 03:27:42 +0900

    feat: ReviewController User Entity parameter

--- src/main/java/com/example/seo_dot/review/controller/ReviewController.java
+++ src/main/java/com/example/seo_dot/review/controller/ReviewController.java
@@ -7,6 +7,7 @@ import com.example.seo_dot.review.dto.request.ReviewCreateRequestDTO;
 import com.example.seo_dot.review.dto.request.ReviewModifyRequestDTO;
 import com.example.seo_dot.review.dto.response.ReviewListResponseDTO;
 import com.example.seo_dot.review.service.ReviewService;
+import com.example.seo_dot.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
@@ -22,27 +23,27 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/api/books/{bookId}/reviews")
-    public ResponseEntity<MessageResponseDTO> createReview(@PathVariable Long bookId, @RequestBody ReviewCreateRequestDTO reviewCreateRequestDTO, Long userId) {
-        MessageResponseDTO response = reviewService.createReview(bookId, reviewCreateRequestDTO, 1L);
+    public ResponseEntity<MessageResponseDTO> createReview(@PathVariable Long bookId, @RequestBody ReviewCreateRequestDTO reviewCreateRequestDTO, User user) {
+        MessageResponseDTO response = reviewService.createReview(bookId, reviewCreateRequestDTO, user);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/api/books/{bookId}/reviews")
-    public ResponseEntity<PageDto<List<ReviewListResponseDTO>>> getReviews(@PathVariable Long bookId, @ModelAttribute ReviewPageParam reviewPageParam, Long userId) {
-        Slice<ReviewListResponseDTO> result = reviewService.getReviews(reviewPageParam, bookId, 1L);
+    public ResponseEntity<PageDto<List<ReviewListResponseDTO>>> getReviews(@PathVariable Long bookId, @ModelAttribute ReviewPageParam reviewPageParam, User user) {
+        Slice<ReviewListResponseDTO> result = reviewService.getReviews(reviewPageParam, bookId, user);
         PageDto<List<ReviewListResponseDTO>> response = new PageDto<>(result.getContent(), result.hasNext());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PutMapping("/api/books/{bookId}/reviews/{reviewId}")
-    public ResponseEntity<MessageResponseDTO> modifyReview(@PathVariable Long bookId, @PathVariable Long reviewId, @RequestBody ReviewModifyRequestDTO reviewModifyRequestDTO, Long userId) {
-        MessageResponseDTO response = reviewService.modifyReview(bookId, reviewId, reviewModifyRequestDTO, 1L);
+    public ResponseEntity<MessageResponseDTO> modifyReview(@PathVariable Long bookId, @PathVariable Long reviewId, @RequestBody ReviewModifyRequestDTO reviewModifyRequestDTO, User user) {
+        MessageResponseDTO response = reviewService.modifyReview(bookId, reviewId, reviewModifyRequestDTO, user);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping("/api/books/{bookId}/reviews/{reviewId}")
-    public ResponseEntity<MessageResponseDTO> deleteComment(@PathVariable Long bookId, @PathVariable Long reviewId, Long userId) {
-        MessageResponseDTO response = reviewService.deleteReview(bookId, reviewId, 1L);
+    public ResponseEntity<MessageResponseDTO> deleteComment(@PathVariable Long bookId, @PathVariable Long reviewId, User user) {
+        MessageResponseDTO response = reviewService.deleteReview(bookId, reviewId, user);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
