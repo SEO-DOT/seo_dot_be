506d365fe2ad4321c95cb6aaf637e1301a4b218a
kki4504 <rhkddlf9876@gmail.com>
2023-11-10 21:47:43 +0900

    feat: user token controller

--- src/main/java/com/example/seo_dot/user/controller/UserController.java
+++ src/main/java/com/example/seo_dot/user/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.example.seo_dot.user.controller;
 
+import com.example.seo_dot.user.domain.User;
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    @GetMapping("/test")
+    public String getUser() {
+        return "Hello World";
+    }
 }
