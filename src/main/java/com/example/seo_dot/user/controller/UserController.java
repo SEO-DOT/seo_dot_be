7db78e823292ce47056ce827075930ab728f9f10
Sihwan Yu <tlghks1217@naver.com>
2023-11-13 16:11:28 +0900

    feat: 카카오 사용자 정보 가져오기 구현

--- src/main/java/com/example/seo_dot/user/controller/UserController.java
+++ src/main/java/com/example/seo_dot/user/controller/UserController.java
@@ -1,14 +1,34 @@
 package com.example.seo_dot.user.controller;
 
-import com.example.seo_dot.user.domain.User;
-import lombok.Getter;
+import com.example.seo_dot.global.jwt.JwtUtil;
+import com.example.seo_dot.user.service.KakaoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+
+    private final KakaoService kakaoService;
+
     @GetMapping("/test")
     public String getUser() {
         return "Hello World";
+    }
+
+    @GetMapping("/user/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        String token = kakaoService.kakaoLogin(code);
+
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, token);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/";
     }
 }
