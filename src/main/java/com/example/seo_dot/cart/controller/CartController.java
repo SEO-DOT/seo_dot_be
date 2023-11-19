8d68a74cb10f0a28fa04fd0d9e4d945c369d5ae7
Dsys1129 <dsys1129@gmail.com>
2023-11-19 15:58:56 +0900

    feat: addCart, getCartSize, Controller, Service, Repository 생성

--- src/main/java/com/example/seo_dot/cart/controller/CartController.java
+++ src/main/java/com/example/seo_dot/cart/controller/CartController.java
@@ -1,4 +1,39 @@
 package com.example.seo_dot.cart.controller;
 
+import com.example.seo_dot.cart.dto.CartRequestDTO;
+import com.example.seo_dot.cart.dto.CartResponseDTO;
+import com.example.seo_dot.cart.service.CartService;
+import com.example.seo_dot.global.dto.MessageResponseDTO;
+import com.example.seo_dot.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+@Service
 public class CartController {
+
+    private final CartService cartService;
+
+//    @GetMapping("/api/cart")
+//    public ResponseEntity<?> getCart(Long userId) {
+//        cartService.getCart(userId);
+//        return null;
+//    }
+
+    @PostMapping("/api/cart")
+    public ResponseEntity<MessageResponseDTO> addCart(@RequestBody CartRequestDTO requestDTO, User user) {
+        MessageResponseDTO response = cartService.addCart(requestDTO, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/api/cart/size")
+    public ResponseEntity<CartResponseDTO> getCartSize(Long userId) {
+        CartResponseDTO response = cartService.getCartSize(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
--- src/main/java/com/example/seo_dot/cart/service/CartService.java
+++ src/main/java/com/example/seo_dot/cart/service/CartService.java
@@ -1,4 +1,41 @@
 package com.example.seo_dot.cart.service;
 
+import com.example.seo_dot.book.domain.Book;
+import com.example.seo_dot.book.repository.BookRepository;
+import com.example.seo_dot.cart.dto.CartRequestDTO;
+import com.example.seo_dot.cart.domain.Cart;
+import com.example.seo_dot.cart.dto.CartResponseDTO;
+import com.example.seo_dot.cart.repository.CartRepository;
+import com.example.seo_dot.global.dto.MessageResponseDTO;
+import com.example.seo_dot.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class CartService {
+
+    private final CartRepository cartRepository;
+    private final BookRepository bookRepository;
+
+//    public void getCart(Long userId) {
+//        cartRepository.
+//    }
+
+    public CartResponseDTO getCartSize(Long userId) {
+        Integer cartSize = cartRepository.countByUserId(userId);
+        return CartResponseDTO.createCartResponseStatus200(cartSize);
+    }
+
+
+    @Transactional
+    public MessageResponseDTO addCart(CartRequestDTO requestDTO, User user) {
+        Book book = bookRepository.findById(requestDTO.getBookId())
+                .orElseThrow(() -> new IllegalArgumentException());
+        Cart cart = new Cart(user, book,requestDTO.getQuantity());
+        cartRepository.save(cart);
+
+        return MessageResponseDTO.createSuccessMessage201();
+    }
 }
