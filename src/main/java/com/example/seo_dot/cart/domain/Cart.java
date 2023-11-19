df80dc23f3ceac69fe3023a6ef4d3e6dde8d9ea0
Dsys1129 <dsys1129@gmail.com>
2023-11-19 15:56:53 +0900

    feat: Cart Entity, CartBook Entity 생성

--- src/main/java/com/example/seo_dot/cart/domain/Cart.java
+++ src/main/java/com/example/seo_dot/cart/domain/Cart.java
@@ -1,11 +1,36 @@
 package com.example.seo_dot.cart.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.example.seo_dot.book.domain.Book;
+import com.example.seo_dot.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "carts")
 @Entity
 public class Cart {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private int stock;
+
+    public Cart(User user, Book book, int stock) {
+        this.id = id;
+        this.user = user;
+        this.book = book;
+        this.stock = stock;
+    }
 }
---
+++ src/main/java/com/example/seo_dot/cart/domain/CartBook.java
@@ -0,0 +1,23 @@
+package com.example.seo_dot.cart.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "carts_books")
+@Entity
+public class CartBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    private int stock;
+}
