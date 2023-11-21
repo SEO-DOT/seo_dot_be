bd2d527573ba068f0f7d88fd4f7c0255a820b2d9
Dsys1129 <dsys1129@gmail.com>
2023-11-21 18:44:22 +0900

    feat: Cart Controller 구현

--- src/main/java/com/example/seo_dot/cart/controller/CartController.java
+++ src/main/java/com/example/seo_dot/cart/controller/CartController.java
@@ -1,39 +1,54 @@
 package com.example.seo_dot.cart.controller;
 
-import com.example.seo_dot.cart.dto.CartRequestDTO;
-import com.example.seo_dot.cart.dto.CartResponseDTO;
+import com.example.seo_dot.cart.dto.request.CartQuantityUpdateDTO;
+import com.example.seo_dot.cart.dto.request.CartRequestDTO;
+import com.example.seo_dot.cart.dto.response.CartListResponseDTO;
+import com.example.seo_dot.cart.dto.response.CartQuantityResponseDTO;
+import com.example.seo_dot.cart.dto.response.CartResponseDTO;
 import com.example.seo_dot.cart.service.CartService;
-import com.example.seo_dot.global.dto.MessageResponseDTO;
-import com.example.seo_dot.user.domain.User;
+import com.example.seo_dot.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
-@Service
+@RestController
 public class CartController {
 
     private final CartService cartService;
 
-//    @GetMapping("/api/cart")
-//    public ResponseEntity<?> getCart(Long userId) {
-//        cartService.getCart(userId);
-//        return null;
-//    }
-
     @PostMapping("/api/cart")
-    public ResponseEntity<MessageResponseDTO> addCart(@RequestBody CartRequestDTO requestDTO, User user) {
-        MessageResponseDTO response = cartService.addCart(requestDTO, user);
+    public ResponseEntity<CartResponseDTO> addCart(@RequestBody CartRequestDTO requestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        log.info("addCart");
+        CartResponseDTO response = cartService.addCart(requestDTO, userDetails.getUser());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @GetMapping("/api/cart")
+    public ResponseEntity<CartListResponseDTO> getCartList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CartListResponseDTO response = cartService.getCartList(userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @GetMapping("/api/cart/size")
-    public ResponseEntity<CartResponseDTO> getCartSize(Long userId) {
-        CartResponseDTO response = cartService.getCartSize(userId);
+    public ResponseEntity<CartResponseDTO> getCartSize(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CartResponseDTO response = cartService.getCartSize(userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/api/cart/{cartId}")
+    public ResponseEntity<CartResponseDTO> deleteCartItem(@PathVariable Long cartId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CartResponseDTO response = cartService.deleteCartItem(cartId, userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PutMapping("/api/cart/update-qty")
+    public ResponseEntity<CartQuantityResponseDTO> updateCartItemQuantity(@RequestBody CartQuantityUpdateDTO requestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CartQuantityResponseDTO response = cartService.updateCartItemQuantity(requestDTO, userDetails.getUser());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
