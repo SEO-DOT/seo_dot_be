1de0d6143e3a06ceff39d58beaf16142d1b71523
Dsys1129 <dsys1129@gmail.com>
2023-11-24 16:23:24 +0900

    feat: MainPage bestReview API 추가

--- src/main/java/com/example/seo_dot/book/controller/MainPageController.java
+++ src/main/java/com/example/seo_dot/book/controller/MainPageController.java
@@ -3,6 +3,8 @@ package com.example.seo_dot.book.controller;
 import com.example.seo_dot.book.dto.response.BookListResponseDTO;
 import com.example.seo_dot.book.dto.response.NewBookListResponseDTO;
 import com.example.seo_dot.book.service.BookService;
+import com.example.seo_dot.review.dto.response.BestReviewListResponseDTO;
+import com.example.seo_dot.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.List;
 public class MainPageController {
 
     private final BookService bookService;
+    private final ReviewService reviewService;
 
     @GetMapping("/api/welcome/best")
     public ResponseEntity<List<BookListResponseDTO>> getBestSellers() {
@@ -26,6 +29,12 @@ public class MainPageController {
     @GetMapping("/api/welcome/new")
     public ResponseEntity<List<NewBookListResponseDTO>> getNewBooks() {
         List<NewBookListResponseDTO> response = bookService.getNewBooks();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/api/welcome/bestReviews")
+    public ResponseEntity<List<BestReviewListResponseDTO>> getBestReviews() {
+        List<BestReviewListResponseDTO> response = reviewService.getBestReviews();
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
