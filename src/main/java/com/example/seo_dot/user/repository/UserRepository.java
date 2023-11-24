127cfe7a221f9193b11cc6eb7efb209ee28d0c61
Dsys1129 <dsys1129@gmail.com>
2023-11-24 16:12:24 +0900

    fix: User Entity 필드 추가 및 updateMethod 추가

--- src/main/java/com/example/seo_dot/user/repository/UserRepository.java
+++ src/main/java/com/example/seo_dot/user/repository/UserRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByNickName(String nickName);
+    boolean existsByNickname(String nickName);
+
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
