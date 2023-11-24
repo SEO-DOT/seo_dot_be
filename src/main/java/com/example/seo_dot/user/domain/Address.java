e72f4bf00ce67a03cc8721ebd345c1eeb00a10e3
Dsys1129 <dsys1129@gmail.com>
2023-11-24 16:07:31 +0900

    fix: Getter, Constructor 추가

--- src/main/java/com/example/seo_dot/user/domain/Address.java
+++ src/main/java/com/example/seo_dot/user/domain/Address.java
@@ -1,10 +1,22 @@
 package com.example.seo_dot.user.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Address {
     private String zoneCode;
     private String streetAddress;
     private String detailAddress;
+
+    public Address(String zoneCode, String streetAddress, String detailAddress) {
+        this.zoneCode = zoneCode;
+        this.streetAddress = streetAddress;
+        this.detailAddress = detailAddress;
+    }
 }
