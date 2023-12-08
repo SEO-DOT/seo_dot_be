70602d196e594558f7b08547b1ee4aea57f4ce13
Sihwan Yu <tlghks1217@naver.com>
2023-12-08 23:50:28 +0900

    feat:bookmark 기능 추가 및 bookmarkCount 필드 Book Entity에 추가

--- src/main/java/com/example/seo_dot/bookmark/controller/BookmarkController.java
+++ src/main/java/com/example/seo_dot/bookmark/controller/BookmarkController.java
@@ -1,4 +1,25 @@
 package com.example.seo_dot.bookmark.controller;
 
+import com.example.seo_dot.bookmark.service.BookmarkService;
+import com.example.seo_dot.global.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bookmark")
+@RequiredArgsConstructor
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @PostMapping("/{bookId}")
+    public ResponseEntity addBookmark(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long bookId) {
+        bookmarkService.addBookmark(userDetails.getUser(), bookId);
+        return ResponseEntity.ok().build();
+    }
 }
